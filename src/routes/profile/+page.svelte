e8077ee01f4b78a401f0e2ae09910d8912297c57
Brogoming <kotag@outlook.com>
2023-07-22 23:14:56 -0500

    Working on login input/output
    I'm trying to make sure that we are getting the values from the user. I'm having a problem with get the data from the API. I used some of the pseudo code but some times it would give me undefined on the page so I got rid of the if/else statements and alerts. I made a method that checks the inputs all at once for now, I would like to have it update everytime the user clicks away from the text box and a small message from that text box would appear to say whats wrong.

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -2,36 +2,116 @@
 	import { gql } from '@apollo/client/core';
 	import client from '../../apollo.js';
 
-	let userInput = '',
-		emailInput = '',
+	let emailInput = '',
+		userInput = '',
 		passInput = '';
+
+	let emailRes = {},
+		usernameRes = {},
+		passwordRes = {};
 
 	const handleClick = () => {
 		userInput = document.getElementById('userInput').value;
 		emailInput = document.getElementById('emailInput').value;
 		passInput = document.getElementById('passInput').value;
-		console.log(userInput, emailInput, passInput);
+		checkEmail(emailInput);
+		checkUsername(userInput);
+		checkPassword(emailInput, userInput, passInput);
+		login();
 	};
 
 	//main function that calls the other functions in order
 	function login() {
-		pass;
+		console.log(userInput, emailInput, passInput);
+	}
+
+	async function checkEmail(email) {
+		try {
+			const myres = client.query({
+				Method: 'POST',
+				variables: {
+					email: email
+				},
+				query: gql`
+					query checkEmail($email: citext!) {
+						net_users_logins(where: { email: { _eq: $email } }) {
+							email
+							password
+							username
+							passwordrequired
+						}
+					}
+				`
+			});
+
+			emailRes = (await myres).data.net_users_logins[0];
+
+			return (await myres).data.net_users_logins[0];
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	async function checkUsername(username) {
+		try {
+			const myres = client.query({
+				Method: 'POST',
+				variables: {
+					username: username
+				},
+				query: gql``
+			});
+
+			return (await myres).data;
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	async function addAccount(email, username) {
+		try {
+			const myres = client.query({
+				Method: 'POST',
+				variables: {
+					email: email,
+					username: username
+				},
+				query: gql``
+			});
+
+			return (await myres).data;
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	async function checkPassword(email, username, password) {
+		try {
+			const myres = client.query({
+				Method: 'POST',
+				variables: {
+					email: email,
+					username: username,
+					password: password
+				},
+				query: gql``
+			});
+
+			return (await myres).data;
+		} catch (err) {
+			console.log(err);
+		}
 	}
 </script>
 
 <h1>Login Page</h1>
-<div>
-	<div>
-		<label for="userInput">Username:</label>
-		<input type="text" id="userInput" onSubmit={handleClick} />
-	</div>
-	<div>
-		<label for="emailInput">Email:</label>
-		<input type="email" id="emailInput" onSubmit={handleClick} />
-	</div>
-	<div>
-		<label for="passInput">Password:</label>
-		<input type="password" id="passInput" onSubmit={handleClick} />
-	</div>
-	<button on:click={handleClick}>Submit</button>
-</div>
+<form on:submit={handleClick}>
+	<label for="emailInput">Email:</label>
+	<input type="email" id="emailInput" required value={emailInput} />
+	<label for="userInput">Username:</label>
+	<input type="text" id="userInput" required value={userInput} />
+	<label for="passInput">Password:</label>
+	<input type="password" required id="passInput" value={passInput} />
+	
+	<button on:submit={handleClick}>Submit</button>
+</form>
